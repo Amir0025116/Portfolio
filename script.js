fd92c28ee5a57b0530d79b5e3266b5d8bcabde71
Amir0025116 <amirnasikhov@gmail.com>
2023-05-13 17:06:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const serviseOp = document.querySelector('.servises-opacity')  
+const myButton = document.querySelector('.button')  
+
+ 
+
+myButton.addEventListener('click',  function()  {
+    serviseOp.classList.toggle('active')
+})    
+    myButton.addEventListener('click', function () {
+        myButton.classList.toggle('button')
+    })
+ 
+
+ 
